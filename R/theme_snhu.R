e87dac3879d2ac828d8b5906e74b87fb268434e2
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-06-22 15:13:01 -0400

    updated theme options

--- R/theme_snhu.R
+++ R/theme_snhu.R
@@ -6,17 +6,15 @@ theme_snhu <- function(base_size = 11,
   theme_minimal(base_size = base_size,
                 base_family = base_family,
                 base_line_size = base_line_size) %+replace%
-    theme(
-      plot.title = element_text(
-        size = rel(1.2),
-        face = "bold",
-        hjust = 0.5),
-      # axis.title = element_text(
-      #   color = rgb(105, 105, 105, maxColorValue = 255),
-      #   size = rel(0.75)),
-      # axis.text = element_text(
-      #   color = rgb(105, 105, 105, maxColorValue = 255),
-      #   size = rel(0.5)),
-      complete = TRUE
-    )
+  theme(
+    plot.title = element_text(size = rel(1.2), face = "bold", hjust = 0.5, vjust = 2),
+    plot.subtitle = element_text(size = rel(1.05), face = "bold", hjust = 0.5, vjust = 1.5),
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(),
+    strip.background = element_rect(fill = "black"),
+    strip.text = element_text(color = "white", face = "bold", size = rel(1)),
+    legend.title = element_blank(),
+    legend.position = "bottom",
+    complete = TRUE
+  )
 }
