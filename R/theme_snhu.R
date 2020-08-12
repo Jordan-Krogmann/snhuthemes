063756b1662a07855a4ca7a5850fc3ac1a3d4323
Jordan <52896238+Jordan-Krogmann@users.noreply.github.com>
2020-08-12 14:52:51 -0400

    edit

--- R/theme_snhu.R
+++ R/theme_snhu.R
@@ -1,8 +1,9 @@
 
 theme_snhu <- function(base_size = 11,
-                      base_family = "",
-                      base_line_size = base_size / 170,
-                      base_rect_size = base_size / 170){
+                       base_family = "",
+                       base_line_size = base_size / 170,
+                       base_rect_size = base_size / 170){
+
   theme_minimal(base_size = base_size,
                 base_family = base_family,
                 base_line_size = base_line_size) %+replace%
